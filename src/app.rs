6683508450684c4617aff85ee1827074791ec4e1
Matt Ware <mattbware3@gmail.com>
2024-03-18 04:08:10 +0000

    Options menu
    Theme is only option for now.
    Also, theme choice is now persistent

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use egui::Align;
+use egui::{Align, Visuals};
 use egui_modal::Modal;
 
 /// We derive Deserialize/Serialize so that we can persist app state on shutdown.
@@ -7,12 +7,15 @@ use egui_modal::Modal;
 pub struct DifficultyEditorApp {
     #[serde(skip)]
     new_difficulty: String,
+    #[serde(skip)]
+    dark_mode_enabled: bool,
 }
 
 impl Default for DifficultyEditorApp {
     fn default() -> Self {
         Self {
             new_difficulty: "New Difficulty".to_owned(),
+            dark_mode_enabled: true,
         }
     }
 }
@@ -24,6 +27,11 @@ impl DifficultyEditorApp {
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             // cc.egui_ctx.send_viewport_cmd(ViewportCommand::Maximized(true));
+            if eframe::get_value(storage, "dark_mode").unwrap_or(true) {
+                cc.egui_ctx.set_visuals(Visuals::dark())
+            } else {
+                cc.egui_ctx.set_visuals(Visuals::light())
+            }
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
 
@@ -67,9 +75,26 @@ impl eframe::App for DifficultyEditorApp {
             });
         });
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
+        let options_modal = Modal::new(ctx, "options_modal").with_close_on_outside_click(true);
+        options_modal.show(|ui| {
+            options_modal.title(ui, "Options");
+            options_modal.frame(ui, |ui| {
+                egui::ScrollArea::vertical().auto_shrink(true).max_height(400.0).show(ui, |ui| {
+                    ui.horizontal(|ui| {
+                        ui.label("Theme: ");
+                        let mut visuals = ui.ctx().style().visuals.clone();
+                        visuals.light_dark_radio_buttons(ui);
+                        self.dark_mode_enabled = visuals.dark_mode;
+                        ui.ctx().set_visuals(visuals);
+                    });
+                });
+            });
+            options_modal.buttons(ui, |ui| {
+                options_modal.button(ui, "Close");
+            });
+        });
 
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 // NOTE: no File->Quit on web pages!
                 ui.menu_button("File", |ui| {
@@ -86,6 +111,13 @@ impl eframe::App for DifficultyEditorApp {
                     }
                 });
 
+                ui.menu_button("Tools", |ui| {
+                    if ui.button("Options").clicked() {
+                        ui.close_menu();
+                        options_modal.open();
+                    }
+                });
+
                 ui.menu_button("Help", |ui| {
                     if ui.button("About").clicked() {
                         ui.close_menu();
@@ -95,7 +127,6 @@ impl eframe::App for DifficultyEditorApp {
 
                 ui.add_space(16.0);
 
-                egui::widgets::global_dark_light_mode_buttons(ui);
                 ui.with_layout(egui::Layout::right_to_left(Align::Max), |ui| {
                     ui.add_space(16.0);
                     egui::warn_if_debug_build(ui);
@@ -111,6 +142,7 @@ impl eframe::App for DifficultyEditorApp {
     /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
+        eframe::set_value(storage, "dark_mode", &self.dark_mode_enabled);
     }
 }
 
